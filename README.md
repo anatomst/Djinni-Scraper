2202183056a6d11f9a0d0bca744b639c592f0e3e
antonshpachuk <www.shpachyk98@gmail.com>
2022-11-02 18:42:41 +0200

    add readme.md

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Djinni-Scraper
+
+The idea of Djinni Scraper is to help you to understand the most demanded technologies on the tech market right now
+
+## Installation
+
+Python must be already installed
+
+```shell
+git clone https://github.com/anatomst/Djinni-Scraper.git
+python3 -m venv venv
+source venv/bin/activate (on Linux and macOS) or venv\Scripts\activate (on Windows)
+pip install -r requirements.txt
+
+# to get csv file with all python vacancies (~20 seconds)
+python3 djinni_parser.py
+
+# to get graphic bar
+python3 python data_python.py "*csv_file" 
+# *csv_file - csv file you want to convert to bar
+# example: python data_python.py "python_vacancies-2022-11-02-18:2.csv" 
+# now you have png in data_png directory
+```
+
+## Screenshots 
+
+![Website Interface](demo.png)
+
+![Website Interface](demo1.png)
---
+++ data_python.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import sys
+import pandas as pd
+import ast
+from matplotlib import rcParams
+
+
+def main(file_name: str):
+    df = pd.read_csv(file_name)
+    df_columns = ["title", "technologies", "experience"]
+    df = df.loc[:, df_columns].copy()
+    df = df.dropna()
+    technologies_list = [ast.literal_eval(technologies) for technologies in df["technologies"]]
+    df["technologies"] = technologies_list
+
+    # df_technologies_columns = [technology for technologies in df["technologies"] for technology in technologies]
+    # df_technologies_columns = list(dict.fromkeys(df_technologies_columns))
+
+    technologies_flat = df["technologies"].explode()
+
+    df = df.join(pd.crosstab(technologies_flat.index, technologies_flat))
+    df = df.drop(columns=["experience", "title", "technologies"])
+
+    now = datetime.now().today()
+    png_name = f"./data_png/top-python-technologies-{now.date()}.png"
+    rcParams.update({'figure.autolayout': True})
+    top_technologies = df.sum().sort_values(ascending=False).iloc[:28]
+    top_technologies.plot.bar(title=f'The most demanded technologies \n {now.date()}').figure.savefig(png_name)
+
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    main(file_name)
